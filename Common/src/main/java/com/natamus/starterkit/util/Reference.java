33b76e8b7d738a2c09157a41aa4adc0e7098b713
Rick South <rick@natamus.com>
2023-12-12 00:01:25 +0100

    Starter Kit source code for 1.19.4.

---
+++ Common/src/main/java/com/natamus/starterkit/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.starterkit.util;
+
+public class Reference {
+	public static final String MOD_ID = "starterkit";
+	public static final String NAME = "Starter Kit";
+	public static final String VERSION = "5.2";
+	public static final String ACCEPTED_VERSIONS = "[1.19.4]";
+}

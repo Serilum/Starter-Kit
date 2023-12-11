9d32f4e640bcf4e52371e817fb96be2b7103302b
Rick South <rick@natamus.com>
2023-12-12 00:01:41 +0100

    Starter Kit source code for 1.20.2.

---
+++ Forge/src/main/java/com/natamus/starterkit/ModForge.java
@@ -0,0 +1,36 @@
+package com.natamus.starterkit;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.starterkit.forge.config.IntegrateForgeConfig;
+import com.natamus.starterkit.forge.events.ForgeFirstSpawnEvent;
+import com.natamus.starterkit.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+    	MinecraftForge.EVENT_BUS.register(new ForgeFirstSpawnEvent());
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
---
+++ NeoForge/src/main/java/com/natamus/starterkit/ModNeoForge.java
@@ -0,0 +1,36 @@
+package com.natamus.starterkit;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.starterkit.neoforge.config.IntegrateNeoForgeConfig;
+import com.natamus.starterkit.neoforge.events.NeoForgeFirstSpawnEvent;
+import com.natamus.starterkit.util.Reference;
+import net.neoforged.neoforge.common.NeoForge;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModNeoForge {
+	
+	public ModNeoForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateNeoForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		NeoForge.EVENT_BUS.register(NeoForgeFirstSpawnEvent.class);
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}

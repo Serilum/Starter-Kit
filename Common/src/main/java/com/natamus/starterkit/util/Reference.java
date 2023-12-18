6df91c21a91b243a76476ab6a5a6f8605eec7085
Rick South <rick@natamus.com>
2023-12-18 10:00:20 +0100

    (v5.4) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/starterkit/util/Reference.java
+++ Common/src/main/java/com/natamus/starterkit/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.starterkit.util;
 public class Reference {
 	public static final String MOD_ID = "starterkit";
 	public static final String NAME = "Starter Kit";
-	public static final String VERSION = "5.3";
+	public static final String VERSION = "5.4";
 	public static final String ACCEPTED_VERSIONS = "[1.20.4]";
 }

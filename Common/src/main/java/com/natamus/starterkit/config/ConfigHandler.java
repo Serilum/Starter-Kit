741012f6dcfa4ed1b1a0e892a534fa99b883b8e7
Rick South <rick@natamus.com>
2023-12-31 16:10:33 +0100

    (v5.5) Added enablePlayerMustBeNearSpawnForKitCheck config option. [+1]

--- Common/src/main/java/com/natamus/starterkit/config/ConfigHandler.java
+++ Common/src/main/java/com/natamus/starterkit/config/ConfigHandler.java
@@ -10,10 +10,14 @@ import java.util.List;
 public class ConfigHandler extends DuskConfig {
 	public static HashMap<String, List<String>> configMetaData = new HashMap<String, List<String>>();
 
+	@Entry public static boolean enablePlayerMustBeNearSpawnForKitCheck = true;
 	@Entry public static boolean addExistingItemsAfterKitSet = true;
 	@Entry public static boolean enableFTBIslandCreateCompatibility = true;
 
 	public static void initConfig() {
+		configMetaData.put("enablePlayerMustBeNearSpawnForKitCheck", Arrays.asList(
+			"If a player that hasn't logged in before must be near spawn to be given a kit. This can be useful for installing the mod on existing worlds."
+		));
 		configMetaData.put("addExistingItemsAfterKitSet", Arrays.asList(
 			"Whether items that existed in the inventory, such as books added by other mods, should be added back to the inventory after the kit was set. If disabled, they'll be removed. You can still manually set them via the kit."
 		));

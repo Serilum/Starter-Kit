221cc83ee31da77d20b4fff7408a694d4b210b6d
Rick South <rick@natamus.com>
2024-02-29 15:06:57 +0100

    (v6.1) Adds backwards compatibility when updating from Starter Kit version 5.5 to 6.0+.

--- Common/src/main/java/com/natamus/starterkit/util/Reference.java
+++ Common/src/main/java/com/natamus/starterkit/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.starterkit.util;
 public class Reference {
 	public static final String MOD_ID = "starterkit";
 	public static final String NAME = "Starter Kit";
-	public static final String VERSION = "6.0";
+	public static final String VERSION = "6.1";
 	public static final String ACCEPTED_VERSIONS = "[1.20.4]";
 }

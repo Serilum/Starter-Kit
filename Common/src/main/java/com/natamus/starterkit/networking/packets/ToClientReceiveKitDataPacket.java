6bad570e5084f83db79fecf14a7d20be8834ed24
Rick South <rick@natamus.com>
2026-09-22 16:03:17 +0200

    Updated Starter Kit to Minecraft version 26.3.0.

--- Common/src/main/java/com/natamus/starterkit/networking/packets/ToClientReceiveKitDataPacket.java
+++ Common/src/main/java/com/natamus/starterkit/networking/packets/ToClientReceiveKitDataPacket.java
@@ -6,6 +6,7 @@ import com.natamus.collective.implementations.networking.data.Side;
 import com.natamus.starterkit.data.VariablesClient;
 import com.natamus.starterkit.util.Reference;
 import net.minecraft.network.FriendlyByteBuf;
+import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.resources.Identifier;
 
 import java.util.HashMap;
@@ -22,16 +23,16 @@ public class ToClientReceiveKitDataPacket {
     }
 
     public static ToClientReceiveKitDataPacket decode(FriendlyByteBuf buf) {
-        HashMap<String, String> entriesIn = buf.readMap(Maps::newHashMapWithExpectedSize, FriendlyByteBuf::readUtf, FriendlyByteBuf::readUtf);
-        HashMap<String, String> descriptionsIn = buf.readMap(Maps::newHashMapWithExpectedSize, FriendlyByteBuf::readUtf, FriendlyByteBuf::readUtf);
+        HashMap<String, String> entriesIn = ByteBufCodecs.map(Maps::newHashMapWithExpectedSize, ByteBufCodecs.STRING_UTF8, ByteBufCodecs.STRING_UTF8).decode(buf);
+        HashMap<String, String> descriptionsIn = ByteBufCodecs.map(Maps::newHashMapWithExpectedSize, ByteBufCodecs.STRING_UTF8, ByteBufCodecs.STRING_UTF8).decode(buf);
 
         return new ToClientReceiveKitDataPacket(entriesIn, descriptionsIn);
     }
 
     public void encode(FriendlyByteBuf buf) {
         // Due to ambiguity on NeoForge, the lambda's are replaced
-        buf.writeMap(packetStarterGearEntries, (a, b) -> a.writeUtf(b), (a, b) -> a.writeUtf(b)); // FriendlyByteBuf::writeUtf, FriendlyByteBuf::writeUtf
-        buf.writeMap(packetStarterKitDescriptions, (a, b) -> a.writeUtf(b), (a, b) -> a.writeUtf(b)); // FriendlyByteBuf::writeUtf, FriendlyByteBuf::writeUtf
+        ByteBufCodecs.map(Maps::newHashMapWithExpectedSize, ByteBufCodecs.STRING_UTF8, ByteBufCodecs.STRING_UTF8).encode(buf, packetStarterGearEntries); // FriendlyByteBuf::writeUtf, FriendlyByteBuf::writeUtf
+        ByteBufCodecs.map(Maps::newHashMapWithExpectedSize, ByteBufCodecs.STRING_UTF8, ByteBufCodecs.STRING_UTF8).encode(buf, packetStarterKitDescriptions); // FriendlyByteBuf::writeUtf, FriendlyByteBuf::writeUtf
     }
 
     public static void handle(PacketContext<ToClientReceiveKitDataPacket> ctx) {
